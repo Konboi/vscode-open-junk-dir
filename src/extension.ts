2bd9835e3e4ff53d8abab2420633f1e1144918b9
Konboi <ryosuke.yabuki@gmail.com>
2019-01-20 10:08:04 +0900

    get dir name from  momentjs

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as moment from 'moment';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,11 +17,19 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.openJunkDir', () => {
 		// The code you place here will be executed every time your command is executed
 
-		let rootDir = vscode.workspace.getConfiguration("openJunkDir").get<string>("junkFileDir") || "~/junk"
-		vscode.window.showInformationMessage("rootDir:"+  rootDir);
+		let rootDir = vscode.workspace.getConfiguration("openJunkDir").get<string>("junkFileDir")
+
+
+		let fileDirFormat = vscode.workspace.getConfiguration("openJunkDir").get<string>("junkFileFormat")
+		let fileDir = moment().format(fileDirFormat)
 
 		// Display a message box to the user
+		vscode.window.showInformationMessage("rootDir:"+  rootDir + " " +  fileDir);
 		vscode.window.showInformationMessage('Hello Open Junk Dir');
+
+		console.log(moment().format(fileDir));
+		console.log(moment().format('YYYY-MM-DD'))
+
 	});
 
 	context.subscriptions.push(disposable);
