3f2cef5b110d51d18dbea5f94d6153ca81367eb6
Konboi <ryosuke.yabuki@gmail.com>
2019-01-26 16:40:22 +0900

    support directory name

--- src/extension.ts
+++ src/extension.ts
@@ -12,13 +12,25 @@ export function activate(context: vscode.ExtensionContext) {
       .get<string>("junkFileFormat");
 
     let dir = await vscode.window.showInputBox({
-      prompt: "Directory Suffix"
+      prompt: "Directory Name"
     })
     let junkDir = untildify(rootDir + moment().format(fileDirFormat) + "_" + dir)
 
     if (!fs.existsSync(junkDir)) {
       fs.mkdirsSync(junkDir)
     }
+
+    let file = await vscode.window.showInputBox({
+      prompt: "File Name",
+    })
+
+    let uri = vscode.Uri.parse("untitled:" + junkDir + "/" + file)
+
+    vscode.workspace.openTextDocument(uri).then((text) => {
+      vscode.window.showTextDocument(text, vscode.ViewColumn.One)
+    })
+    console.log("open:" + junkDir)
   });
+
   context.subscriptions.push(disposable);
 }
